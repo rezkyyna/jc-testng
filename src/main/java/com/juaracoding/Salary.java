61ec6e4eab8dac78009a005e5a57791db3b114db
rezkyyna <rezkykarunia8@gmail.com>
2023-09-30 08:40:27 +0700

    Feature Salary Test

--- src/main/java/com/juaracoding/Salary.java
+++ src/main/java/com/juaracoding/Salary.java
@@ -7,16 +7,16 @@ public class Salary {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Masukkan gaji karyawan: ");
+        System.out.print("Masukkan Gaji Karyawan: $");
         double gaji = input.nextDouble();
 
         double pajak = hitungPajak(gaji);
 
         double gajiBersih = gaji - pajak;
 
-        System.out.println("Gaji karyawan: $" + gaji);
-        System.out.println("Pajak yang dikenakan: $" + pajak);
-        System.out.println("Gaji bersih: $" + gajiBersih);
+        System.out.println("Gaji Karyawan: $" + gaji);
+        System.out.println("Pajak yang Dikenakan: $" + pajak);
+        System.out.println("Gaji Bersih: $" + gajiBersih);
 
         input.close();
     }
